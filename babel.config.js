4a67aaa67b627d29144abd335e3b403c1f026bb1
aminya <aminyahyaabadi74@gmail.com>
2021-07-23 18:49:27 -0500

    use babel-preset-atomic

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,13 @@
-let presets = [];
-
-let plugins = [
-  ["@babel/plugin-proposal-optional-chaining", { loose: false }],
-  ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: false }],
-];
-
-if (process.env.BABEL_ENV === "development") {
-  plugins.push(...[
-    "@babel/plugin-transform-modules-commonjs",
-    "@babel/plugin-proposal-export-namespace-from",
-  ]);
-}
-
 module.exports = {
-  presets: presets,
-  plugins: plugins,
+  presets: [
+    [
+      "babel-preset-atomic",
+      {
+        flow: false,
+        react: false,
+      },
+    ],
+  ],
   exclude: "node_modules/**",
   sourceMap: "inline",
-};
+}
